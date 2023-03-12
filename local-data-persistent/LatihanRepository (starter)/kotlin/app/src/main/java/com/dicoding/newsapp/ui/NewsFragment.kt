ceb96fdc685fec4c89b77c31aa410963fd6d545f
puxxbu <pusswarog@gmail.com>
2023-03-12 19:47:56 +0700

    init commit

--- local-data-persistent/LatihanRepository (starter)/kotlin/app/src/main/java/com/dicoding/newsapp/ui/NewsFragment.kt
+++ local-data-persistent/LatihanRepository (starter)/kotlin/app/src/main/java/com/dicoding/newsapp/ui/NewsFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.dicoding.newsapp.data.Result
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.newsapp.databinding.FragmentNewsBinding
 
@@ -24,8 +27,50 @@ class NewsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         tabName = arguments?.getString(ARG_TAB)
 
-        val newsAdapter = NewsAdapter()
-        
+        val factory: ViewModelFactory = ViewModelFactory.getInstance(requireActivity())
+        val viewModel: NewsViewModel by viewModels {
+            factory
+        }
+
+
+        val newsAdapter = NewsAdapter { news ->
+            if (news.isBookmarked){
+                viewModel.deleteNews(news)
+            } else {
+                viewModel.saveNews(news)
+            }
+        }
+
+        if (tabName == TAB_NEWS) {
+            viewModel.getHeadlineNews().observe(viewLifecycleOwner) { result ->
+                if (result != null) {
+                    when (result) {
+                        is Result.Loading -> {
+                            binding?.progressBar?.visibility = View.VISIBLE
+                        }
+                        is Result.Success -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            val newsData = result.data
+                            newsAdapter.submitList(newsData)
+                        }
+                        is Result.Error -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            Toast.makeText(
+                                context,
+                                "Terjadi kesalahan" + result.error,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        } else if (tabName == TAB_BOOKMARK) {
+            viewModel.getBookmarkedNews().observe(viewLifecycleOwner) { bookmarkedNews ->
+                binding?.progressBar?.visibility = View.GONE
+                newsAdapter.submitList(bookmarkedNews)
+            }
+        }
+
         binding?.rvNews?.apply {
             layoutManager = LinearLayoutManager(context)
             setHasFixedSize(true)
